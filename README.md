eb30e16f604ef83c11017b646a7b8b71cc2f8243
js <jad.sayegh@isae.edu.lb>
2021-02-26 03:04:09 +0300

    Init Commit: Generation du projet avec Spring Initializr avec les dependences Web et MongoDB

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# C12021SBL
+# 8403f_jadsayegh_c1_2021
+
+Service Web RESTful en Utilisant Spring Boot avec une BDD MongoDB
---
+++ src/test/java/com/c12021sbl/Users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c12021sbl.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
